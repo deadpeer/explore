ec69941dc20f1fda5ed057e6f0291aa958bec97f
deadpeer <deadpeer@github.com>
2019-06-20 11:40:56 -0500

    add emitter

--- src/web/index.js
+++ src/web/index.js
@@ -1,3 +1,20 @@
+// next
+// TODO: stream.fromFuture
+// TODO: stream.awaitFutures
+// TODO: future.fold
+// TODO: future.value
+// TODO: record (object)
+
+// future
+// TODO: add typescript
+// TODO: fork crocks (typescript + monet IO)
+// TODO: fork most (use IO)
+// TODO: fork fluture (use IO)
+// TODO: copy useful lodash / ramda functions
+// TODO: integrate crocks + most + fluture into library
+// TODO: ensure proper code splitting
+// TODO: fork prettier to implement functional code style
+
 import 'setimmediate'
 
 import h from 'mutant/html-element'
@@ -6,7 +23,9 @@ import send from 'mutant/send'
 import computed from 'mutant/computed'
 import when from 'mutant/when'
 import monet from 'monet'
+import crocks from 'crocks'
 import { pipe, compose } from 'ramda'
+import ee from 'event-emitter'
 import {
   of as mostOf,
   map as mostMap,
@@ -14,6 +33,7 @@ import {
   ap as mostAp,
   slice as mostSlice,
   reduce as mostReduce,
+  fromEvent as mostFromEvent,
   merge,
   mergeArray,
   combine,
@@ -30,7 +50,6 @@ import {
   delay,
   multicast,
   timestamp,
-  tap,
   filter,
   skipRepeats,
   skipRepeatsWith,
@@ -49,7 +68,6 @@ import {
   scan,
   from,
   fromPromise,
-  fromEvent,
   periodic,
   empty,
   never,
@@ -76,143 +94,196 @@ import { seed } from './seed.js'
 
 import './index.css'
 
-const { Either, Left, Right, IO } = monet
+const { IO } = monet
+const { Either } = crocks
+
+const Left = Either.Left
+const Right = Either.Right
 
 // functional
-const noop = () => {}
+const noop =
+  () => {}
 
-const useless = () => IO(noop)
+const useless =
+  () => IO (noop)
 
-const of = M => v =>
-  M.of
-    ? M.of(v)
-    : M.unit
-    ? M.unit(v)
-    : M.pure
-    ? M.pure(v)
-    : M === Future
-    ? resolve(v)
-    : M === Promise
-    ? Promise.resolve(v)
-    : null
+const id =
+  v => () => v
 
-const chain = f => m =>
-  m.chain
-    ? m.chain(f)
-    : m.flatMap
-    ? m.flatMap(f)
-    : m.bind
-    ? m.bind(f)
-    : m.then
-    ? m.then(f)
-    : isFuture(m)
-    ? futureChain(f)(m)
-    : null
+const of =
+  M => v =>
+    M.of
+      ? M . of (v)
+      : M.unit
+      ? M . unit (v)
+      : M.pure
+      ? M . pure (v)
+      : M === Future
+      ? resolve (v)
+      : M === Promise
+      ? Promise . resolve (v)
+      : null
 
-const reduce = r => i => m =>
+const chain =
+  f => m =>
+    m.chain
+      ? m . chain (f)
+      : m.flatMap
+      ? m . flatMap (f)
+      : m.bind
+      ? m . bind (f)
+      : m.then
+      ? m . then (f)
+      : isFuture (m)
+      ? futureChain (f) (m)
+      : null
+
+const reduce =
+  r => i => m =>
   isStream(m)
-    ? () => reduceStream(r)(i)(m)
+    ? reduceStream (r) (i) (m)
     : m.reduce
-    ? () => m.reduce(r, i)
+    ? () => m . reduce (r, i)
     : null
 
-const ap = f => m => (isFuture(m) ? futureAp(f)(m) : m.ap(f))
+const ap =
+  f => m =>
+    isFuture (m)
+      ? futureAp (f) (m)
+      : m.ap (f)
 
-const map = f => m => (isFuture(m) ? futureMap(f)(m) : m.map(f))
+const map =
+  f => m => (isFuture (m) ? futureMap (f) (m) : m . map (f))
 
 // io
-IO.io = f => IO(f)
+const run =
+  io => io.run ()
 
-IO.of = f => IO(f)
+const runWith =
+  f => v => run (f (v))
 
-IO.unit = f => IO(f)
+const isIO =
+  m => !!m.effectFn
 
-IO.pure = f => IO(f)
+const Return =
+  value => IO (() => value)
 
-const run = io => io.run()
+const Log =
+  value => IO (() => console . log (value))
+
+const Report =
+  error => IO (() => console . error (error))
 
 // either
-const isRight = e => e.isRight()
+const isRight = e =>
+  e . isRight ()
 
-const isLeft = e => e.isLeft()
+const isLeft =
+  e => e . isLeft ()
 
-const right = e => e.right()
+const right =
+  e => e . right ()
 
-const left = e => e.left()
+const left =
+  e => e . left ()
 
-const value = e => {
-  let x
+const value =
+  e => {
+    let x
 
-  try {
-    x = right(e)
-  } catch (error) {
-    x = left(e)
-  }
-
-  return x
-}
-
-const containsLeft = v => e => v === left(e)
-
-const containsRight = v => e => v === right(e)
-
-const contains = v => e => {
-  let x
-
-  try {
-    x = right(e)
-  } catch (error) {
-    x = left(e)
-  }
-
-  return x === v
-}
-
-// flow (do notation)
-const flow = M => g => {
-  const doing = g()
-
-  const recurse = v => {
-    const current = doing.next(v)
-
-    if (current.done) {
-      return of(M)(current.value)
+    try {
+      x = right (e)
+    } catch (error) {
+      x = left (e)
     }
 
-    return chain(recurse)(current.value)
+    return x
   }
 
-  return recurse()
-}
+const containsLeft =
+  v => e => v === left (e)
+
+const containsRight =
+  v => e => v === right (e)
+
+const contains =
+  v => e => {
+    let x
+
+    try {
+      x = right (e)
+    } catch (error) {
+      x = left (e)
+    }
+
+    return x === v
+  }
+
+// flow (do notation)
+const flow =
+  M => g => {
+    const doing = g ()
+
+    const recurse = v => {
+      const current = doing . next (v)
+
+      if (current.done) {
+        return of (M) (current.value)
+      }
+
+      return chain (recurse) (current.value)
+    }
+
+    return recurse ()
+  }
 
 // reactive
-const ERROR_UNHANDLED_EXCEPTION = 'Unhandled exception in stream.'
-
 const fork = reject => resolve => future =>
-  of(IO)(() =>
-    futureFork(e => run(reject(e)))(v => run(resolve(v)))(future),
-  ).map(cancel => () => IO(() => cancel()))
+  map (
+    cancel => IO (() => cancel ())
+  ) (
+    IO (() => futureFork (
+      isIO (reject) ? () => run (reject) : runWith (reject)
+    ) (
+      isIO (resolve) ? () => run (resolve) : runWith (resolve)
+    ) (future))
+  )
 
 const subscribe = observer => stream =>
-  IO(() =>
-    stream.subscribe({
-      next: value => run((observer.next || useless)(value)),
-      complete: () => run((observer.complete || useless)()),
-      error: error => run(observer.error(error)),
-    }),
-  ).map(subscription => () => IO(() => subscription.unsubscribe()))
+  map (
+    subscription => IO (() => subscription.unsubscribe ())
+  ) (
+    IO (() =>
+      stream . subscribe ({
+        error: error => run (observer . error (error)),
+        next: value => run ((observer.next || useless) (value)),
+        complete: () => run ((observer.complete || useless)),
+      })
+    )
+  )
 
-// TODO: const drain = () => {}
+const drain =
+  stream => IO (() => stream . drain ()) . map (promise => encaseP (() => promise) ())
 
-const observe = createIO => stream =>
-  of(IO)(() => stream.observe(v => run(createIO(v)))).map(promise =>
-    encaseP(() => promise)(),
+const observe =
+  f => stream => map (
+    promise => encaseP (() => promise) ()
+  ) (
+    IO (
+      () => stream . observe (
+        v => run (f (v))
+      )
+    )
   )
 
 const isStream = stream => !!stream.source
 
-const reduceStream = reducer => initial => stream =>
-  encaseP(() => mostReduce(reducer, initial, stream))()
+const reduceStream =
+  reducer => initial => stream =>
+    encaseP (() => mostReduce (reducer, initial, stream)) ()
+
+const fromEvent =
+  type => emitter => mostFromEvent (type, emitter)
 
 const Stream = {
   of: mostOf,
@@ -222,52 +293,106 @@ const Stream = {
   slice: mostSlice,
 }
 
+class EmitterInstance {
+  emitEvent (type) {
+    return value => IO (() => this . _emit (type, value))
+  }
+
+  addEventListener (type, listener) {
+    this . on (type, listener)
+  }
+
+  removeEventListener (type, listener) {
+    this . off (type, listener)
+  }
+}
+
+ee (EmitterInstance.prototype)
+
+EmitterInstance.prototype._emit = EmitterInstance.prototype.emit
+EmitterInstance.prototype.emit = EmitterInstance.prototype.emitEvent
+
+const Emitter =
+  () => new EmitterInstance ()
+
+const emit =
+  emitter => type => value => emitter . emit (type) (value)
+
 // math
 const ERROR_DIVIDE_BY_ZERO = 'Cannot divide by zero.'
 
-const multiply = x => y => x * y
-const add = x => y => x + y
-const divide = x => y => {
-  if (x === 0) throw new Error(ERROR_DIVIDE_BY_ZERO)
+const multiply =
+  x => y => x * y
 
-  return y / x
-}
+const add =
+  x => y => x + y
+
+const divide =
+  x => y => {
+    if (x === 0) throw new Error (ERROR_DIVIDE_BY_ZERO)
+
+    return y / x
+  }
 
 // execution
-const Return = value => IO(() => value)
+const all =
+  (current, previous) => previous + current
 
-const Log = value => IO(() => console.log(value))
+const toUpperCase =
+  string => string.toUpperCase ()
 
-const Report = error => IO(() => console.error(error))
-
-const all = (current, previous) => previous + current
-
-const process = pipe(
-  multiply(10),
-  add(50),
-  divide(5),
+const process = pipe (
+  multiply (10),
+  add (50),
+  divide (5),
 )
 
-const main = flow(IO)(function*() {
-  const string = yield Return('hello')
+const io = Return (20) . map (add (15)) . map (divide (5))
 
-  yield Log(string)
+const other = pipe (map (add (10))) (Return (20))
 
-  // const future = yield fork (Report) (Return) (
-  //   reduce (all) (0) (
-  //     from ([1, 2, 3, 4, 5]) . map (process)
-  //   )
-  // )
+const main = flow (IO) (function * () {
+  const string = yield Return ('hello')
+
+  yield Log (string)
+
+  const x = yield io
+  const y = yield other
+
+  const stream = from ([1, 2, 3, 4, 5, x, y])
+
+  const future = reduce (all) (0) (stream . map (process))
+
+  const unsubscribe = yield subscribe ({
+    next: Log,
+    error: Report,
+    complete: Log ('done'),
+  }) (stream)
+
+  const cancel = yield fork (Report) (Log) (future)
+
+  // yield cancel
+  // yield unsubscribe
+
+  const f = yield observe (Log) (stream . map (multiply (2)))
+
+  yield fork (Report ('miss')) (Log ('pass')) (f)
+
+  const emitter = Emitter ()
+
+  const foo = fromEvent ('foo') (emitter) . map (toUpperCase) . take (2)
+
+  yield observe (Log) (foo)
+
+  const emitFoo = emit (emitter) ('foo')
+
+  yield emitFoo ('foo')
+  yield emitFoo ('bar')
+  yield emitFoo ('hello')
+  yield emitFoo ('world')
 })
 
-run(main)
-
-// const main = pipe (
-//   map (add (10)),
-//   chain (Log),
-// ) (Return (20))
-
-// const io = Return (20) . map (add (15)) . chain (Log)
+run (main)
 
 // run (
 //   pipe (
@@ -313,15 +438,15 @@ run(main)
 
 // const io = fork(console.error)(console.log)(future).chain(cancel => cancel())
 
-const state = Struct({
+const state = Struct ({
   text: 'Test',
   color: 'red',
   value: 0,
 })
 
-const isBlue = computed([state.color], color => color === 'blue')
+const isBlue = computed ([state.color], color => color === 'blue')
 
-const element = h(
+const element = h (
   'div.cool',
   {
     classList: ['m-16', 'cool', state.text],
@@ -330,21 +455,27 @@ const element = h(
     },
   },
   [
-    h('div', [state.text, ' ', state.value, ' ', h('strong', 'test')]),
-    h('div', [
-      when(
+    h ('div', [
+      state.text,
+      ' ',
+      state.value,
+      ' ',
+      h ('strong', 'test')
+    ]),
+    h ('div', [
+      when (
         isBlue,
-        h(
+        h (
           'button',
           {
-            'ev-click': send(state.color.set, 'red'),
+            'ev-click': send (state.color.set, 'red'),
           },
           'Change color to red',
         ),
-        h(
+        h (
           'button',
           {
-            'ev-click': send(state.color.set, 'blue'),
+            'ev-click': send (state.color.set, 'blue'),
           },
           'Change color to blue',
         ),
@@ -353,20 +484,23 @@ const element = h(
   ],
 )
 
-setTimeout(function() {
-  state.text.set('Another value')
-}, 5000)
+setTimeout (
+  () => state.text.set ('Another value'),
+  5000,
+)
 
-setInterval(function() {
-  state.value.set(state.value() + 1)
-}, 1000)
+setInterval (
+  () => state.value.set (state.value () + 1),
+  1000,
+)
 
-setInterval(function() {
-  state.set({
+setInterval (
+  () => state.set ({
     text: 'Retrieved from server (not really)',
     color: '#FFEECC',
     value: 1337,
-  })
-}, 10000)
+  }),
+  10000,
+)
 
-document.body.appendChild(element)
+document.body.appendChild (element)
